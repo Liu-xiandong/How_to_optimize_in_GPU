639b5c745d252153d56f0a70562565c05ee0380d
Liu-xiandong <85323580+Liu-xiandong@users.noreply.github.com>
2021-11-27 19:17:24 +0800

    Create reduce_v5_completely_unroll.cu

---
+++ reduce_v5_completely_unroll.cu
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+#include <cuda.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <time.h>
+#include <sys/time.h>
+
+#define THREAD_PER_BLOCK 256
+
+template <unsigned int blockSize>
+__device__ void warpReduce(volatile float* cache, unsigned int tid){
+    if (blockSize >= 64)cache[tid]+=cache[tid+32];
+    if (blockSize >= 32)cache[tid]+=cache[tid+16];
+    if (blockSize >= 16)cache[tid]+=cache[tid+8];
+    if (blockSize >= 8)cache[tid]+=cache[tid+4];
+    if (blockSize >= 4)cache[tid]+=cache[tid+2];
+    if (blockSize >= 2)cache[tid]+=cache[tid+1];
+}
+
+template <unsigned int blockSize>
+__global__ void reduce5(float *d_in,float *d_out){
+    __shared__ float sdata[THREAD_PER_BLOCK];
+
+    // each thread loads one element from global to shared mem
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
+    sdata[tid] = d_in[i] + d_in[i + blockDim.x];
+    __syncthreads();
+
+    // do reduction in shared mem
+    if (blockSize >= 512) {
+        if (tid < 256) { 
+            sdata[tid] += sdata[tid + 256]; 
+        } 
+        __syncthreads(); 
+    }
+    if (blockSize >= 256) {
+        if (tid < 128) { 
+            sdata[tid] += sdata[tid + 128]; 
+        } 
+        __syncthreads(); 
+    }
+    if (blockSize >= 128) {
+        if (tid < 64) { 
+            sdata[tid] += sdata[tid + 64]; 
+        } 
+        __syncthreads(); 
+    }
+    if (tid < 32) warpReduce<blockSize>(sdata, tid);
+
+    // write result for this block to global mem
+    if (tid == 0) d_out[blockIdx.x] = sdata[0];
+}
+
+bool check(float *out,float *res,int n){
+    for(int i=0;i<n;i++){
+        if(out[i]!=res[i])
+            return false;
+    }
+    return true;
+}
+
+int main(){
+    const int N=32*1024*1024;
+    float *a=(float *)malloc(N*sizeof(float));
+    float *d_a;
+    cudaMalloc((void **)&d_a,N*sizeof(float));
+
+    int NUM_PER_BLOCK = 2*THREAD_PER_BLOCK;
+    int block_num = N / NUM_PER_BLOCK;
+    float *out=(float *)malloc(block_num*sizeof(float));
+    float *d_out;
+    cudaMalloc((void **)&d_out,block_num*sizeof(float));
+    float *res=(float *)malloc(block_num*sizeof(float));
+
+    for(int i=0;i<N;i++){
+        a[i]=1;
+    }
+
+    for(int i=0;i<block_num;i++){
+        float cur=0;
+        for(int j=0;j<NUM_PER_BLOCK;j++){
+            cur+=a[i * NUM_PER_BLOCK + j];
+        }
+        res[i]=cur;
+    }
+
+    cudaMemcpy(d_a,a,N*sizeof(float),cudaMemcpyHostToDevice);
+
+    dim3 Grid( block_num, 1);
+    dim3 Block( THREAD_PER_BLOCK, 1);
+
+    reduce5<THREAD_PER_BLOCK><<<Grid,Block>>>(d_a,d_out);
+
+    cudaMemcpy(out,d_out,block_num*sizeof(float),cudaMemcpyDeviceToHost);
+
+    if(check(out,res,block_num))printf("the ans is right\n");
+    else{
+        printf("the ans is wrong\n");
+        for(int i=0;i<block_num;i++){
+            printf("%lf ",out[i]);
+        }
+        printf("\n");
+    }
+
+    cudaFree(d_a);
+    cudaFree(d_out);
+}
