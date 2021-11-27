5122d5968d7c9f11625b27f3ac5d2bbc3d7f83be
Liu-xiandong <85323580+Liu-xiandong@users.noreply.github.com>
2021-11-27 19:14:19 +0800

    Create reduce_v1_no_divergence_branch.cu

---
+++ reduce_v1_no_divergence_branch.cu
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <cuda.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <time.h>
+#include <sys/time.h>
+
+#define THREAD_PER_BLOCK 256
+
+// bank conflict
+__global__ void reduce1(float *d_in,float *d_out){
+    __shared__ float sdata[THREAD_PER_BLOCK];
+
+    // each thread loads one element from global to shared mem
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+    sdata[tid] = d_in[i];
+    __syncthreads();
+
+    // do reduction in shared mem
+    for(unsigned int s=1; s < blockDim.x; s *= 2) {
+        int index = 2 * s * tid;
+        if (index < blockDim.x) {
+            sdata[index] += sdata[index + s];
+        }
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) d_out[blockIdx.x] = sdata[0];
+}
+
+bool check(float *out,float *res,int n){
+    for(int i=0;i<n;i++){
+        if(out[i]!=res[i])
+            return false;
+    }
+    return true;
+}
+
+int main(){
+    const int N=32*1024*1024;
+    float *a=(float *)malloc(N*sizeof(float));
+    float *d_a;
+    cudaMalloc((void **)&d_a,N*sizeof(float));
+
+    int block_num=N/THREAD_PER_BLOCK;
+    float *out=(float *)malloc((N/THREAD_PER_BLOCK)*sizeof(float));
+    float *d_out;
+    cudaMalloc((void **)&d_out,(N/THREAD_PER_BLOCK)*sizeof(float));
+    float *res=(float *)malloc((N/THREAD_PER_BLOCK)*sizeof(float));
+
+    for(int i=0;i<N;i++){
+        a[i]=1;
+    }
+
+    for(int i=0;i<block_num;i++){
+        float cur=0;
+        for(int j=0;j<THREAD_PER_BLOCK;j++){
+            cur+=a[i*THREAD_PER_BLOCK+j];
+        }
+        res[i]=cur;
+    }
+
+    cudaMemcpy(d_a,a,N*sizeof(float),cudaMemcpyHostToDevice);
+
+    dim3 Grid( N/THREAD_PER_BLOCK,1);
+    dim3 Block( THREAD_PER_BLOCK,1);
+
+    reduce1<<<Grid,Block>>>(d_a,d_out);
+
+    cudaMemcpy(out,d_out,block_num*sizeof(float),cudaMemcpyDeviceToHost);
+
+    if(check(out,res,block_num))printf("the ans is right\n");
+    else{
+        printf("the ans is wrong\n");
+        for(int i=0;i<block_num;i++){
+            printf("%lf ",out[i]);
+        }
+        printf("\n");
+    }
+
+    cudaFree(d_a);
+    cudaFree(d_out);
+}
